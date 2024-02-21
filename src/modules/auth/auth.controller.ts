18a3aa5a83a6ee85604e00a37971b628c278f3e5
CesarCr300 <cc30122003@gmail.com>
2024-02-21 17:31:36 -0500

    feat(auth): implementation of jwt in the login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private _service: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req: any) {
-    return req.user;
+    return this._service.login(req.user);
   }
 }
