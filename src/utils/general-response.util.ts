ce6a5b7379063b0c0ac3799196e1d62a090ddaec
CesarCr300 <cesarjcontreras30@gmail.com>
2024-02-22 16:51:56 -0500

    config(general-response): creation of general response entity and util

---
+++ src/utils/general-response.util.ts
@@ -0,0 +1,38 @@
+import { HttpStatus } from '@nestjs/common';
+import { GeneralResponse } from 'src/entities/general-response.entity';
+
+export class GeneralResponseUtil {
+  static Status<T>(
+    message: string,
+    data: T,
+    status: number,
+  ): GeneralResponse<T> {
+    const response = new GeneralResponse<T>();
+    response.message = message;
+    response.data = data;
+    response.status = status;
+    return response;
+  }
+  static OK<T>(message: string, data: T): GeneralResponse<T> {
+    return GeneralResponseUtil.Status<T>(message, data, HttpStatus.OK);
+  }
+
+  static BAD_REQUEST<T>(message: string, data: T): GeneralResponse<T> {
+    return GeneralResponseUtil.Status<T>(message, data, HttpStatus.BAD_REQUEST);
+  }
+
+  static INTERNAL_SERVER_ERROR<T>(
+    message: string,
+    data: T,
+  ): GeneralResponse<T> {
+    return GeneralResponseUtil.Status<T>(
+      message,
+      data,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+
+  static NOT_FOUND<T>(message: string, data: T): GeneralResponse<T> {
+    return GeneralResponseUtil.Status<T>(message, data, HttpStatus.NOT_FOUND);
+  }
+}
