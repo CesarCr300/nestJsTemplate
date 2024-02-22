3fbf6cb27351c5f1313f42fb977e50180e90989d
CesarCr300 <cesarjcontreras30@gmail.com>
2024-02-22 16:42:31 -0500

    refactor: folder configuration deleted and dbconfig created in config folder

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
 
+import { UserModule } from 'src/modules/user/user.module';
+import { dbConfig } from 'src/config/db.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseConfiguration } from 'src/configuration/database.configuration';
-import { UserModule } from 'src/modules/user/user.module';
-import { DataSource } from 'typeorm';
 import { AuthModule } from '../auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(DatabaseConfiguration.get()),
+    TypeOrmModule.forRootAsync(dbConfig),
     UserModule,
     AuthModule,
   ],
