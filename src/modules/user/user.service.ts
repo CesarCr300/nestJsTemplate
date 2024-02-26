0ad94bb5512d193c96eb8fcdaf44af4e7abf89aa
CesarCr300 <cc30122003@gmail.com>
2024-02-25 21:40:03 -0500

    feat(service.base): creating method to update

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -36,9 +36,9 @@ export class UserService extends ServiceBase<
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} user`;
