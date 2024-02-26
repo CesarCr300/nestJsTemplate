3d5657a8440d5e4676d97af8231bf989d50d85d5
CesarCr300 <cc30122003@gmail.com>
2024-02-25 22:00:56 -0500

    sorting methods in user.module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from './entities/user.entity';
+import { UserType } from './entities/type-user.entity';
+import { UserRepository } from './repository/user.repository';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { User } from './entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from './repository/user.repository';
-import { UserType } from './entities/type-user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, UserType])],
