085bc2e93bd6e1e3e170003c6a9c3ee4f9ea1663
CesarCr300 <cc30122003@gmail.com>
2024-02-25 22:04:52 -0500

    config: modification to exclude not wanted properties in dtos

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ResponseInterceptor } from './interceptors/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.useGlobalInterceptors(new ResponseInterceptor());
 
