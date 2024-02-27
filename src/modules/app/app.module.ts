3890f9cae518179fea3961bcec9bf419bdec99ab
CesarCr300 <cc30122003@gmail.com>
2024-02-27 17:36:13 -0500

    fix(user.interceptor): uncomment the lines to assign the userpayload to the cls

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,15 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { DataSource } from 'typeorm';
+import { ClsModule } from 'nestjs-cls';
 
 import { UserModule } from 'src/modules/user/user.module';
 import { dbConfig } from 'src/config/db.config';
+import { UserInterceptor } from 'src/interceptors/user.interceptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { UserInterceptor } from 'src/interceptors/user.interceptor';
-import { ClsModule } from 'nestjs-cls';
+import { AuditorySuscriber } from '../auditory/suscribers/auditory.suscriber';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { ClsModule } from 'nestjs-cls';
       provide: APP_INTERCEPTOR,
       useClass: UserInterceptor,
     },
+    AuditorySuscriber,
   ],
 })
 export class AppModule {
