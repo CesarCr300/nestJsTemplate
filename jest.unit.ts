7245a575fba6354d892606b22a1a68448140212a
CesarCr300 <cc30122003@gmail.com>
2024-03-01 21:05:02 -0500

    config: change to yarn and coverage configured

---
+++ jest.unit.ts
@@ -0,0 +1,35 @@
+import sharedConfig from './jest.config';
+
+module.exports = {
+  ...sharedConfig,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'cobertura'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!*/node_modules/**',
+    '!<rootDir>/src/main.ts',
+  ],
+  reporters: [
+    'default',
+    'jest-sonar',
+    // [
+    //   '@jest-performance-reporter/core',
+    //   {
+    //     errorAfterMs: 1000,
+    //     warnAfterMs: 500,
+    //     logLevel: 'warn',
+    //     maxItems: 5,
+    //     jsonReportPath: 'performance-report.json',
+    //     csvReportPath: 'performance-report.csv',
+    //   },
+    // ],
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },
+};
