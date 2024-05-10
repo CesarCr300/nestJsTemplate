75dd737b10a6701cdebb3c8433a4f8e537e79c35
CesarCr300 <cc30122003@gmail.com>
2024-05-10 16:46:36 -0500

    refactor(user.service): implementing new service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,6 +6,7 @@ import { UserRepository } from './repository/user.repository';
 import { ServiceBase } from '../../base/service.base';
 import { User } from './entities/user.entity';
 import { FilterUserDto } from './dto/filter-user.dto';
+import { HashingUtil } from '../../utils/hashing';
 
 @Injectable()
 export class UserService extends ServiceBase<
@@ -14,16 +15,30 @@ export class UserService extends ServiceBase<
     type: User;
   },
   User,
+  User,
+  User,
   FilterUserDto,
   CreateUserDto,
   UpdateUserDto
 > {
   constructor(_usersRepository: UserRepository) {
-    super(
-      _usersRepository,
-      (dto: CreateUserDto) => ({ email: dto.email }),
-      'el',
-      'usuario',
-    );
+    super(_usersRepository, {
+      article: 'el',
+      resourceName: 'usuario',
+      requiresValidationInUpdate: true,
+      requiresValidationInCreation: true,
+      adapterFindAll: (e: User) => e,
+      adapterFindOne: (e: User) => e,
+      functionToCreateObjectToFindIfTheEntityAlreadyExists: (dto: any) => ({
+        email: dto.email,
+      }),
+    });
+  }
+  async update(id: number, dto: UpdateUserDto): Promise<any> {
+    if (dto.password) {
+      dto.password = await HashingUtil.hash(dto.password);
+    }
+
+    return await super.update(id, dto);
   }
 }
