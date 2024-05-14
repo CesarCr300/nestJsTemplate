2da60444eb649bca21302ef5272f48a09d10e48b
CesarCr300 <cc30122003@gmail.com>
2024-05-14 16:38:52 -0500

    build: adding .env variables for the API port and the Front URL

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from 'process';
 
 import { AppModule } from './modules/app/app.module';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: env.FRONTEND_URL,
+    methods: 'GET,POST,DELETE,PATCH, OPTIONS',
+    allowedHeaders: '*',
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
@@ -22,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 bootstrap();
