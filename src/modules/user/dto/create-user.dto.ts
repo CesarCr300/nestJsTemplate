451ea65b485c1c21450e499a7e427841fdcbe03f
Cesar <cesarjcontreras30@gmail.com>
2024-06-14 18:00:40 -0500

    fix(user): adding fields to be able to create user

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,8 +1,29 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   email: string;
   @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  phoneNumber: number;
+
+  @IsNumber()
+  @IsOptional()
+  userTypeId: number | undefined;
 }
